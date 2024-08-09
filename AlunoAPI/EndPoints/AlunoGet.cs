7586a4aa5972258c043e648b98e14e591af3d7e6
Ryan-Temoteo <ryan.t.temoteo2005@gmail.com>
2024-08-09 20:33:23 -0300

    Primeiro commit

---
+++ AlunoAPI/EndPoints/AlunoGet.cs
@@ -0,0 +1,32 @@
+﻿using AlunoAPI.Entities;
+using AlunoAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunoAPI.EndPoints
+{
+    public class AlunoGet
+    {
+        // Rota definida
+        public static string? Template => "/aluno/{id:int}";
+        public static string[]? Metodo => new string[] { HttpMethod.Get.ToString() };
+
+        public static Delegate Funcao => Acao;
+
+        public static IResult Acao([FromRoute] int id)
+        {
+            AlunoRepository alunocarga = new AlunoRepository();
+            alunocarga.Criar();
+
+            Aluno encontrado = AlunoRepository.BancoDeDados.FirstOrDefault(a => a.Id.Equals(id));
+
+            if (alunocarga != null)
+            {
+                return Results.Ok(encontrado);
+            }
+            else
+            {
+                return Results.NotFound();
+            }
+        }
+    }
+}
---
+++ AlunoAPI/Repository/AlunoRepository.cs
@@ -0,0 +1,22 @@
+﻿using AlunoAPI.Entities;
+
+namespace AlunoAPI.Repository
+{
+    public class AlunoRepository
+    {
+        public static List<Aluno> BancoDeDados { get; set; } = new List<Aluno>();
+
+        public void Criar()
+        {
+            Aluno aluno1 = new Aluno(1 , "Ryan", "Matriculado", 3245);
+            Aluno aluno2 = new Aluno(2 , "João", "Desmatriculado", 7865);
+            Aluno aluno3 = new Aluno(3 , "Aline", "Matriculado", 9876);
+            Aluno aluno4 = new Aluno(4, "Rafael", "Desmatriculado", 9075);
+
+            BancoDeDados.Add(aluno1);
+            BancoDeDados.Add(aluno2);
+            BancoDeDados.Add(aluno3);
+            BancoDeDados.Add(aluno4);
+        }
+    }
+}
